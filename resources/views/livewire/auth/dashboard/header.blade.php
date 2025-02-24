6ff462547bca60c67e7e82db99e541ba2918a3e8
user <user@users.noreply.github.com>
2025-02-24 18:16:25 +1100

    svg render issues

--- resources/views/livewire/auth/dashboard/header.blade.php
+++ resources/views/livewire/auth/dashboard/header.blade.php
@@ -1,7 +1,8 @@
-<section x-data="{ open: false, darkMode: localStorage.getItem('theme') === 'dark' }"
+<section x-data="{ darkMode: localStorage.getItem('theme') === 'dark' }"
          x-init="darkMode = localStorage.getItem('theme') === 'dark';"
          class="border-b bg-primary dark:bg-primary border-secondary">
     <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+
         <div class="flex items-center justify-between h-16">
 
             <!-- name -->
@@ -35,22 +36,7 @@
                 </a>
             </div>
 
-            <!-- Dark / Light Mode Toggle Button -->
-            <div x-data="{ darkMode: localStorage.getItem('theme') === 'dark' }"
-                x-init="darkMode = localStorage.getItem('theme') === 'dark';"
-                class="ml-4">
-                <button @click="darkMode = !darkMode; localStorage.setItem('theme', darkMode ? 'dark' : 'light')">
-                    <!-- Sun icon for dark mode -->
-                    <div x-show="darkMode" class="w-6 h-6">
-                        <img id="moonIcon" alt="Moon Icon" src="/path/to/dark-mode-icon.svg">
-                    </div>
-                    <!-- Moon icon for light mode -->
-                    <div x-show="!darkMode" class="w-6 h-6">
-                        <img id="sunIcon" alt="Sun Icon" src="/path/to/light-mode-icon.svg">
-                    </div>
-                </button>
-            </div>
-
         </div>
+
     </div>
 </section>
